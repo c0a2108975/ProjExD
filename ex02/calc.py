99319d87e220fd01a3faa936f500c0083933bcf4
shoya <c0a2108975@edu.teu.ac.jp>
2022-11-29 15:06:29 +0900

    練習5

--- ex02/calc.py
+++ ex02/calc.py
@@ -10,7 +10,8 @@ root.geometry("300x500")
 entry = tk.Entry(root,justify="right",width = 10,font= ("",40))
 entry.grid(row=0,column=0,columnspan=3)
 
-r,c = 0,0
+r,c = 1,0
+
 for num in range(9,-1,-1):
     button = tk.Button(root,text=f"{num}",width  = 4,height = 2,font=("",30))
     button.grid(row=r,column=c)
@@ -19,5 +20,12 @@ for num in range(9,-1,-1):
     if c%3 == 0:
         r +=1
         c = 0
+operators = ["+","="]
+r,c = 4,1
+for ope in operators:
+    button = tk.Button(root,text=f"{ope}",width = 4,height = 2,font=("",30))
+    button.grid(row=r,column=c)
+    button.bind("<1>",button_click)
+    c += 1
 
 root.mainloop()
