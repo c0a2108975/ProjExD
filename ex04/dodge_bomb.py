a91a8f660054d220f4c73480e6fffb2ac442266f
shoya <c0a2108975@edu.teu.ac.jp>
2022-12-13 15:46:10 +0900

    3限基本機能実装完了

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -1,10 +1,23 @@
 import pygame as pg
 import sys
+import random
+
+def check_bound(obj_rct,scr_rct):
+    yoko,tate = 1,1
+    if obj_rct.left < scr_rct.left or scr_rct.right < obj_rct.right:
+        yoko = -1
+    
+    if obj_rct.top < scr_rct.top or scr_rct.bottom < obj_rct.bottom:
+        tate = -1
+    return yoko,tate
+
+
 
 def main():
     clock = pg.time.Clock()
     pg.display.set_caption("逃げろ!こうかとん")
     scrn_sfc = pg.display.set_mode((1600,900))
+    scrn_rct = scrn_sfc.get_rect()
     pgbg_sfc = pg.image.load("ex04/pg_bg.jpg")
     pgbg_rct = pgbg_sfc.get_rect()
 
@@ -14,6 +27,15 @@ def main():
     tori_rct.center = 900,400
     scrn_sfc.blit(tori_sfc,tori_rct)
 
+    bomb_sfc = pg.Surface((20,20))
+    bomb_sfc.set_colorkey((0,0,0))
+    pg.draw.circle(bomb_sfc,(255,0,0),(10,10),10)
+    bomb_rct = bomb_sfc.get_rect()
+    bomb_rct.centerx = random.randint(0,1600)
+    bomb_rct.centery = random.randint(0,900)
+    scrn_sfc.blit(bomb_sfc,bomb_rct)
+    vx,vy = 1,1
+    
     while True:
         scrn_sfc.blit(pgbg_sfc,pgbg_rct)
         for event in pg.event.get():
@@ -33,7 +55,28 @@ def main():
         if key_dct[pg.K_RIGHT]:
             tori_rct.centerx += 1
 
+        if check_bound(tori_rct,scrn_rct) != (1,1):
+            if key_dct[pg.K_UP]:
+                tori_rct.centery += 1
+        
+            if key_dct[pg.K_DOWN]:
+                tori_rct.centery -= 1
+            
+            if key_dct[pg.K_LEFT]:
+                tori_rct.centerx += 1
+
+            if key_dct[pg.K_RIGHT]:
+                tori_rct.centerx -= 1
+
+
         scrn_sfc.blit(tori_sfc,tori_rct)
+        bomb_rct.move_ip(vx,vy)
+        scrn_sfc.blit(bomb_sfc,bomb_rct)
+        yoko,tate = check_bound(bomb_rct, scrn_rct)
+        vx *= yoko
+        vy *= tate
+        if tori_rct.colliderect(bomb_rct):
+            return
         pg.display.update()
         clock.tick(1000)
 
